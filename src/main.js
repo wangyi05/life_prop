945fd0af8490dfe8f5626ebd7356cabf2dcf8efe
ywang101 <ywang101@stevens.edu>
2018-11-11 15:46:49 -0500

    adding redux partial

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
-import Marketing from '../app/market.jsx';
-import RealTime from '../app/realtime.jsx';
-import Translate from '../app/translate.jsx';
+//import Marketing from '../app/market.jsx';
+//import RealTime from '../app/realtime.jsx';
+//import Translate from '../app/translate.jsx';
 
-import styles from '../public/assets/bootstrap.min.css';
-import style from '../public/assets/main.css';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { render } from 'react-dom';
+import configureStore from './store/configureStore';
+import App from './App.jsx';
+
+const store = configureStore();
+
+render(
+  <Provider store={store}>
+      <App />
+  </Provider>,
+  document.getElementById('app')
+);
